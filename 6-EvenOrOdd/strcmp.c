a8a92a05692924c96ff56a87c00c851833823071
Olivier Girardot <olive_girardot@hotmail.com>
2024-03-28 10:46:06 +0100

    added string compare function

---
+++ 6-EvenOrOdd/strcmp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char string1[] = "Hello World";
+  char string2[] = "Hello Mum";
+  printf("%d\n", strcmp(string1, string2));
+
+  return 0;
+}
